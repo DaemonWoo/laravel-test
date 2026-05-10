4e8a35e4886030777ba6d285594d678f7d9f056f
DaemonWoo <cofenito@gmail.com>
2026-05-10 19:02:23 +0300

    update env example

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,13 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @hasSection('title')
 
-            <title>@yield('title') - {{ config('app.name') }}</title>
+            <title>{{ config('app.name') }}</title>
         @else
             <title>{{ config('app.name') }}</title>
         @endif
-
-        <!-- Favicon -->
-		<link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
